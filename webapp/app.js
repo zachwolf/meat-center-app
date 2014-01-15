9ad7ec9a9d04ce58d4ab429a097ca261e47a79db
Zach Wolf <hello@zachwolf.com>
2014-01-14 23:08:26 -0600

    Hello world app.js

--- webapp/app.js
+++ webapp/app.js
@@ -1,1 +1,14 @@
-console.log("hello, world!");
+var express = require('express'),
+    app = express();
+
+app.get('/', function(req, res){
+    res.send('Hello World');
+});
+
+app.get('*', function(req, res){
+    res.send('Page Not Found', 404);
+});
+
+app.listen(1337);
+console.log('Express server started on port 1337');
+
