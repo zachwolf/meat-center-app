5d50a0e71be9cd153b8668941008fc5462c53f77
Zach Wolf <hello@zachwolf.com>
2014-01-15 21:34:59 -0600

    tinkering

--- webapp/app.js
+++ webapp/app.js
@@ -12,7 +12,7 @@ var util = require('util'),
 console.log(util.inspect(app.use, { showHidden: true, depth: null }));
 
 app.get('/', function(req, res){
-    res.send('Hello World!!!');
+    res.send('Hello World!');
 });
 
 app.get('*', function(req, res){
