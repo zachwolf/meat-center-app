fe0c0c69ac462aaf4ec6a06182b499ce922c25b9
Zach Wolf <hello@zachwolf.com>
2014-01-25 15:05:56 -0600

    adds compass:dist task

--- grunt-tasks/asset-tasks/compass.task.js
+++ grunt-tasks/asset-tasks/compass.task.js
@@ -43,78 +43,37 @@ module.exports = function (grunt) {
     // console.log("------------------ /compass:dev  --------------------");
   });
 
+  grunt.registerTask('compass:dist', "build local to :dist settings", function () {
+    // console.log("------------------  compass:dist  --------------------");
+    
+    var conf = {};
 
-    /*
+    // clear out styles directory, or files won't be rebuilt with alternative settings
+    grunt.task.run("clean:styles");
 
-    compass: {
-      dist: {
-        options: {
-          outputStyle: 'compressed',
-          noLineComments: true
-        }
-      },
-      dev: {
+    conf = {
+      task : {
         options: {
           sassDir : SETTINGS.STYLE_SOURCE_PATH,
           cssDir  : SETTINGS.STYLE_BUILD_PATH,
-          outputStyle: 'expanded',
-          watch: false
+          outputStyle : 'compressed',
+          noLineComments: true,
+          watch : false
         }
       }
-    },
+    };
 
-    */
+    // create a config.rb file for compass
+    createConfig();
+
+    // run compass
+    grunt.config('compass', conf);
+    grunt.task.run("compass");
+
+    // delete config.rb file for compass
+    deleteConfig();
+
+    // console.log("------------------ /compass:dist  --------------------");
+  });
 
 };
-
-
-// var conf = {};
-
-// console.log("watch:dev called");
-
-// // clean out and recompile all of BUILD_PATH
-// grunt.task.run("build:dev");
-
-// // start a local server
-// // grunt.task.run("server:start");
-// grunt.task.run("open:local");
-
-// conf = {
-//   // reload the page when things change
-//   options: { livereload: true },
-//   // process style sheets on change
-//   styles: {
-//     files: [
-//       SETTINGS.STYLE_SOURCE_PATH + '/*.scss',
-//       SETTINGS.STYLE_SOURCE_PATH + '/**/*.scss'
-//     ],
-//     tasks: [
-//       'compass:dev'
-//     ]
-//   },
-//   // process js on change
-//   scripts: {
-//     files: [
-//       SETTINGS.SCRIPT_SOURCE_PATH + '/*.js',
-//       SETTINGS.SCRIPT_SOURCE_PATH + '/**/*.js'
-//     ],
-//     tasks: [
-//       'jshint:dev',
-//       'karma:unit',
-//       'copy:scripts'
-//     ]
-//   },
-//   // compile html business
-//   markup: {
-//     files: [
-//       SETTINGS.MARKUP_SOURCE_PATH + '/*.handlebars',
-//       SETTINGS.MARKUP_SOURCE_PATH + '/**/*.handlebars'
-//     ],
-//     tasks: [
-//       'copy:markup'
-//     ]
-//   }
-// };
-
-// grunt.config('watch', conf);
-// grunt.task.run("watch");
