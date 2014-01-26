3ea17c52ad3b59d4ce7fa6dfb47d788297e32a09
Zach Wolf <hello@zachwolf.com>
2014-01-26 09:43:36 -0600

    cleaning up debug.task

--- grunt-tasks/debug.task.js
+++ grunt-tasks/debug.task.js
@@ -5,24 +5,29 @@ var util     = require('util'),
 module.exports = function (grunt) {
 
   grunt.registerTask('debug', "throw away function for debugging", function () {
-    // console.log(util.inspect(require, { showHidden: true, depth: null }));
-    // grunt.file.write("test.txt", util.inspect(grunt, { showHidden: true, depth: null }));
-    var done = this.async();
-
-
-    console.log("-------------------------------------------------");
-    // console.log(util.inspect(require("../" + SETTINGS.SERVER_PATH + '/app.js'), { showHidden: true, depth: null }));
-    fs.openSync('./test.txt', 'w');
-    console.log('successfully created test.txt');
-    setTimeout(function () {
-      fs.unlinkSync('./test.txt');
-      console.log('successfully deleted test.txt');
-      done();
-
-    }, 2000);
-
-    console.log("hi");
-    console.log("-------------------------------------------------");
+    console.log("------------------  grunt debug  --------------------");
+    console.log("debug");
+    console.log("------------------ /grunt debug  --------------------");
   });
 
 };
+
+// console.log(util.inspect(require, { showHidden: true, depth: null }));
+// grunt.file.write("test.txt", util.inspect(grunt, { showHidden: true, depth: null }));
+
+/*
+
+// grunt async tasks
+
+var done = this.async();
+
+fs.openSync('./test.txt', 'w');
+console.log('successfully created test.txt');
+setTimeout(function () {
+  fs.unlinkSync('./test.txt');
+  console.log('successfully deleted test.txt');
+  done();
+
+}, 2000);
+
+*/
