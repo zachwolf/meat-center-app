dd84ba1425f8a19617843407824d96a64dc26039
Zach Wolf <hello@zachwolf.com>
2014-01-27 09:58:45 -0500

    nodemon added (again)

--- grunt-tasks/server-tasks/server.task.js
+++ grunt-tasks/server-tasks/server.task.js
@@ -21,6 +21,49 @@ module.exports = function (grunt) {
     console.log("-------------------------------------------------");
     console.log("server:start called");
     console.log("-------------------------------------------------");
+
+    /*
+
+nodemon: {
+      dev: {
+        script: 'app.js',
+        options: {
+          cwd: "webapp",
+          // nodeArgs: ['--debug'],
+          // env: {
+          //   PORT: '5455'
+          // },
+          // omit this property if you aren't serving HTML files and 
+          // don't want to open a browser tab on start
+          callback: function (nodemon) {
+            nodemon.on('log', function (event) {
+              console.log("log nodemon");
+              console.log(event.colour);
+            });
+
+            // opens browser on initial server start
+            nodemon.on('config:update', function () {
+              // Delay before server listens on port
+              console.log("config:update nodemon");
+              // setTimeout(function() {
+              //   require('open')('http://localhost:5455');
+              // }, 1000);
+            });
+
+            // refreshes browser when server reboots
+            nodemon.on('restart', function () {
+              console.log("restart nodemon");
+              // Delay before server listens on port
+              // setTimeout(function() {
+              //   require('fs').writeFileSync('.grunt/rebooted', 'rebooted');
+              // }, 1000);
+            });
+          }
+        }
+      }
+    }
+
+    */
     /*console.log('Started web server on port ' + SETTINGS.SERVER_PORT);
     SERVER.listen(SETTINGS.SERVER_PORT);*/
   });
