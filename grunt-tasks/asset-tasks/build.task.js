bb35fcaa91340c69001e8500daba369b4cb3e609
Zach Wolf <hello@zachwolf.com>
2014-01-29 01:22:20 -0500

    Starts of our build task

--- grunt-tasks/asset-tasks/build.task.js
+++ grunt-tasks/asset-tasks/build.task.js
@@ -2,28 +2,38 @@ var SETTINGS = require("./../settings");
 
 module.exports = function (grunt) {
 
-  grunt.registerTask('build:dev', "build local to :dev settings", function () {
+  grunt.registerTask('build:dev', "build unminified versions of assets", function () {
     console.log("-------------------------------------------------");
     console.log("build:dev called");
     console.log("-------------------------------------------------");
-    /*grunt.task.run("clean");
+    // style tasks
+    grunt.task.run('clean:styles');
     grunt.task.run('compass:dev');
+    
+    // script tasks
+    grunt.task.run('clean:scripts');
     grunt.task.run('jshint:dev');
     grunt.task.run('karma:unit');
-    grunt.task.run('copy:scripts');
-    grunt.task.run('copy:markup');*/
+    // grunt.task.run('browserify:unit');
+
+    // notes
+    grunt.task.run('todos');
+    /*
+    */
   });
 
-  grunt.registerTask('build:dist', "build local to :dist settings", function () {
+  grunt.registerTask('build:dist', "build minified assests for production", function () {
     console.log("-------------------------------------------------");
     console.log("build:dist called");
     console.log("-------------------------------------------------");
-    /*grunt.task.run("clean");
+    /*
+    grunt.task.run("clean");
     grunt.task.run('compass:dist');
     grunt.task.run('jshint:dist');
     grunt.task.run('karma:unit');
     grunt.task.run('requirejs:compile');
-    grunt.task.run('copy:markup');*/
+    grunt.task.run('copy:markup');
+    */
   });
 
 };
