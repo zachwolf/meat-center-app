f6d88b45be1823a4127b32051a446e4b6fb89ac3
Zach Wolf <hello@zachwolf.com>
2014-01-30 23:55:13 -0500

    Removes out watch:dist task

--- grunt-tasks/asset-tasks/watch.task.js
+++ grunt-tasks/asset-tasks/watch.task.js
@@ -57,59 +57,59 @@ module.exports = function (grunt) {
 
   });
 
-  grunt.registerTask('watch:dist', "watch and build local to :dist settings", function () {
+  // grunt.registerTask('watch:dist', "watch and build local to :dist settings", function () {
 
-    var conf = {};
+  //   var conf = {};
 
-    console.log("watch:dist called");
+  //   console.log("watch:dist called");
 
-    // clean out and recompile all of BUILD_PATH
-    grunt.task.run("build:dist");
+  //   // clean out and recompile all of BUILD_PATH
+  //   grunt.task.run("build:dist");
 
-    // start a local server
-    // grunt.task.run("server:start");
-    // grunt.task.run("open:local");
+  //   // start a local server
+  //   // grunt.task.run("server:start");
+  //   // grunt.task.run("open:local");
 
-    conf = {
-      // reload the page when things change
-      options: { livereload: true },
-      // process style sheets on change
-      styles: {
-        files: [
-          SETTINGS.STYLE_SOURCE_PATH + '/*.scss',
-          SETTINGS.STYLE_SOURCE_PATH + '/**/*.scss'
-        ],
-        tasks: [
-          'compass:dist'
-        ]
-      },
-      // process js on change
-      scripts: {
-        files: [
-          SETTINGS.SCRIPT_SOURCE_PATH + '/*.js',
-          SETTINGS.SCRIPT_SOURCE_PATH + '/**/*.js'
-        ],
-        tasks: [
-          'jshint:dist',
-          'karma:unit',
-          'requirejs:compile'
-        ]
-      },
-      // compile html business
-      markup: {
-        files: [
-          SETTINGS.MARKUP_SOURCE_PATH + '/*.handlebars',
-          SETTINGS.MARKUP_SOURCE_PATH + '/**/*.handlebars'
-        ],
-        tasks: [
-          'copy:markup'
-        ]
-      }
-    };
+  //   conf = {
+  //     // reload the page when things change
+  //     options: { livereload: true },
+  //     // process style sheets on change
+  //     styles: {
+  //       files: [
+  //         SETTINGS.STYLE_SOURCE_PATH + '/*.scss',
+  //         SETTINGS.STYLE_SOURCE_PATH + '/**/*.scss'
+  //       ],
+  //       tasks: [
+  //         'compass:dist'
+  //       ]
+  //     },
+  //     // process js on change
+  //     scripts: {
+  //       files: [
+  //         SETTINGS.SCRIPT_SOURCE_PATH + '/*.js',
+  //         SETTINGS.SCRIPT_SOURCE_PATH + '/**/*.js'
+  //       ],
+  //       tasks: [
+  //         'jshint:dist',
+  //         'karma:unit',
+  //         'requirejs:compile'
+  //       ]
+  //     },
+  //     // compile html business
+  //     markup: {
+  //       files: [
+  //         SETTINGS.MARKUP_SOURCE_PATH + '/*.handlebars',
+  //         SETTINGS.MARKUP_SOURCE_PATH + '/**/*.handlebars'
+  //       ],
+  //       tasks: [
+  //         'copy:markup'
+  //       ]
+  //     }
+  //   };
 
-    grunt.config('watch', conf);
-    grunt.task.run("watch");
+  //   grunt.config('watch', conf);
+  //   grunt.task.run("watch");
 
-  });
+  // });
 
 };
