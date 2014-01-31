f15a5f003eb0ee632e3b8e6d4c07ba60aa683495
Zach Wolf <hello@zachwolf.com>
2014-01-30 23:45:07 -0500

    Adds browserify task

---
+++ grunt-tasks/asset-tasks/browserify.task.js
@@ -0,0 +1,24 @@
+var SETTINGS = require("./../settings");
+
+module.exports = function (grunt) {
+
+  // alias task name
+  grunt.task.renameTask("browserify", "_browserify");
+
+  grunt.registerTask('browserify', "compile JS", function () {
+    
+    var conf = {
+      task: {
+        files: {}
+      }
+    };
+
+    // example file
+    conf.task.files[ SETTINGS.SCRIPT_BUILD_PATH + "/src/b.js" ] = SETTINGS.SCRIPT_SOURCE_PATH + "/src/b.js";
+
+    grunt.config('_browserify', conf);
+    grunt.task.run("_browserify");
+
+  });
+
+};
