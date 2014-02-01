6c2a282689741ab0999d26f8a25264a1838237f7
Zach Wolf <hello@zachwolf.com>
2014-02-01 00:34:27 -0500

    Adds grunt prompt start local server task

--- grunt-tasks/asset-tasks/watch.task.js
+++ grunt-tasks/asset-tasks/watch.task.js
@@ -1,23 +1,51 @@
-// todo? grunt prompt
-
 var SETTINGS = require("./../settings");
 
 module.exports = function (grunt) {
 
+  // configure prompt task
+  grunt.registerTask('prompt:serverStart', 'prompt for starting server', function () {
+    var conf = {};
+    
+    conf = {
+      task: {
+        options: {
+          questions: [
+            {
+              config: 'options.startServer',
+              type: 'confirm',
+              default: false,
+              message: 'Start local server?'
+            }
+          ],
+          then: function(results){
+            if( results['options.startServer'] ) {
+              // start a local server
+              grunt.task.run("server:start");
+              grunt.task.run("open");
+            }
+          }
+        }
+      }
+    };
+
+    grunt.config('prompt', conf);
+    grunt.task.run("prompt");
+
+  });
+
+
+  // configure watch task
   grunt.task.renameTask("watch", "_watch");
 
   grunt.registerTask('watch', "Compile code on change", function () {
 
     var conf = {};
 
-    console.log("watch called");
-
     // clean out and recompile all of BUILD_PATH
     // grunt.task.run("build");
 
-    // start a local server
-    // grunt.task.run("server:start");
-    // grunt.task.run("open:local");
+    // prompt if we should start up the local server
+    grunt.task.run("prompt:serverStart");
 
     conf = {
       // reload the page when things change
@@ -41,80 +69,14 @@ module.exports = function (grunt) {
         tasks: [
           'jshint:dev',
           'karma',
-          'copy:scripts'
+          'browserify'
         ]
       }
-      // ,
-      // // compile html business
-      // markup: {
-      //   files: [
-      //     SETTINGS.MARKUP_SOURCE_PATH + '/*.handlebars',
-      //     SETTINGS.MARKUP_SOURCE_PATH + '/**/*.handlebars'
-      //   ],
-      //   tasks: [
-      //     'copy:markup'
-      //   ]
-      // }
     };
 
     grunt.config('_watch', conf);
     grunt.task.run("_watch");
 
   });
-
-  // grunt.registerTask('watch:dist', "watch and build local to :dist settings", function () {
-
-  //   var conf = {};
-
-  //   console.log("watch:dist called");
-
-  //   // clean out and recompile all of BUILD_PATH
-  //   grunt.task.run("build:dist");
-
-  //   // start a local server
-  //   // grunt.task.run("server:start");
-  //   // grunt.task.run("open:local");
-
-  //   conf = {
-  //     // reload the page when things change
-  //     options: { livereload: true },
-  //     // process style sheets on change
-  //     styles: {
-  //       files: [
-  //         SETTINGS.STYLE_SOURCE_PATH + '/*.scss',
-  //         SETTINGS.STYLE_SOURCE_PATH + '/**/*.scss'
-  //       ],
-  //       tasks: [
-  //         'compass:dist'
-  //       ]
-  //     },
-  //     // process js on change
-  //     scripts: {
-  //       files: [
-  //         SETTINGS.SCRIPT_SOURCE_PATH + '/*.js',
-  //         SETTINGS.SCRIPT_SOURCE_PATH + '/**/*.js'
-  //       ],
-  //       tasks: [
-  //         'jshint:dist',
-  //         'karma',
-  //         'requirejs:compile'
-  //       ]
-  //     },
-  //     // compile html business
-  //     markup: {
-  //       files: [
-  //         SETTINGS.MARKUP_SOURCE_PATH + '/*.handlebars',
-  //         SETTINGS.MARKUP_SOURCE_PATH + '/**/*.handlebars'
-  //       ],
-  //       tasks: [
-  //         'copy:markup'
-  //       ]
-  //     }
-  //   };
-
-  //   grunt.config('watch', conf);
-  //   grunt.task.run("watch");
-
-  // });
 
 };
