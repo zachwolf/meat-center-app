300f2997831517e4cb21ef2df67d2fc5c5bd2b05
Zach Wolf <hello@zachwolf.com>
2014-02-02 05:49:36 -0500

    adds file template for node tests

---
+++ grunt-tasks/server-tasks/test.task.js
@@ -0,0 +1,11 @@
+var SETTINGS = require("./../settings");
+
+module.exports = function (grunt) {
+
+  grunt.registerTask('test:server', 'run our server tests', function () {
+    console.log("-------------------------------------------------");
+    console.log("server tests!");
+    console.log("-------------------------------------------------");
+  });
+
+};
