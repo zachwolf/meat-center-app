8ecb5c41d7438f074361afd334aedc597a9d4afd
Zach Wolf <hello@zachwolf.com>
2014-02-08 10:56:03 -0600

    file clean up

--- grunt-tasks/server-tasks/server.task.js
+++ grunt-tasks/server-tasks/server.task.js
@@ -9,45 +9,23 @@ module.exports = function (grunt) {
       task: {
         script: 'app.js',
         options: {
-          cwd: "webapp",
-          // nodeArgs: ['--debug'],
-          // env: {
-          //   PORT: '5455'
-          // },
-          // omit this property if you aren't serving HTML files and 
-          // don't want to open a browser tab on start
+          cwd: "webapp"
+          /*,
+          nodeArgs: ['--debug'],
+          env: {
+            PORT: '1337'
+          },
           callback: function (nodemon) {
-            /*
             nodemon.on('log', function (event) {
-              console.log("------------------  nodemon log  --------------------");
-              console.log(arguments);
-              console.log("------------------ /nodemon log  --------------------");
             });
 
-            // opens browser on initial server start
             nodemon.on('config:update', function () {
-              // Delay before server listens on port
-              console.log("------------------  nodemon config:update  --------------------");
-              console.log(arguments);
-              console.log("------------------ /nodemon config:update  --------------------");
-
-              // setTimeout(function() {
-              //   require('open')('http://localhost:5455');
-              // }, 1000);
             });
 
-            // refreshes browser when server reboots
             nodemon.on('restart', function () {
-              console.log("------------------  nodemon restart  --------------------");
-              console.log(arguments);
-              console.log("------------------ /nodemon restart  --------------------");
-              // Delay before server listens on port
-              // setTimeout(function() {
-              //   require('fs').writeFileSync('.grunt/rebooted', 'rebooted');
-              // }, 1000);
             });
-            */
           }
+          */
         }
       }
     };
