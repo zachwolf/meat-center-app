6172cf9213ea10b5937ce4eeb7cc49ce2cb53666
Zach Wolf <hello@zachwolf.com>
2014-02-08 14:48:07 -0600

    Enables tasks to be run while watch is running.

--- grunt-tasks/concurrent.task.js
+++ grunt-tasks/concurrent.task.js
@@ -1,10 +1,36 @@
-var SETTINGS = require("./settings");
+var SETTINGS = require("./settings"),
+    exec     = require('child_process').exec;
 
 module.exports = function (grunt) {
 
   grunt.registerTask('concurrent:all', 'run and watch our server and our assets at the same time', function () {
     var conf = {};
 
+    // enable input while task is watching
+    process.stdin.resume();
+    process.stdin.setEncoding('utf8');
+    process.stdin.on('data', function (data) {
+
+      data = (data + '').trim().toLowerCase();
+   
+      // log todos
+      if (data === "todos") {
+        exec( 'grunt todos', function(error, stdout, stderror) {
+          if(error) throw error;
+          console.log("stdout", stdout);
+        });
+      }
+   
+      // reset built asset files
+      if (data === "reset_build") {
+        exec( 'grunt reset_build', function(error, stdout, stderror) {
+          if(error) throw error;
+          console.log("stdout", stdout);
+        });
+      }
+    });
+
+    // configure concurrent tasks
     conf = {
       task: ['concurrent:server', 'watch:assets'],
       options: {
